86d6fbc908e8019e3b3bf4b106938d234dedfdbd
A Hashim <ahashim983@yahoo.com>
2020-02-29 17:28:37 +0000

    sign up form

---
+++ signup.php
@@ -0,0 +1,20 @@
+<?php
+    require "header.php"
+?>
+
+    <main>
+        <h1>Sign Up</h1>
+        
+        <form action="includes/signup.php" method="post">
+            <input type="text" name="uid" placeholder="username">
+            <input type="text" name="mail" placeholder="email">
+            <input type="password" name="password" placeholder="password">
+            <input type="password" name="password-repeat" placeholder="confirm password">
+
+            <button type="submit" name="signup-submit">Sign Up</button>
+        </form>
+    </main>
+
+<?php
+    require "footer.php"
+?>
