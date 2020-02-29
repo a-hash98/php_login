0de3f26aa414989647e0261a6cf48be239d4731c
A Hashim <ahashim983@yahoo.com>
2020-02-29 16:07:38 +0000

    skeleton code

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <meta name="description" content="Meta description">
+    <meta name=viewport content="width=device-width, intiial-scale=1">
+    <title></title>
+    </head>
+
+    <body>
+        <header>
+        </header>
+
+    <!-- </body>
+
+</html> -->
