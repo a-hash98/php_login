30c253898d28e9559cb4abf924e009c900340a86
A Hashim <ahashim983@yahoo.com>
2020-02-29 17:52:11 +0000

    database handler with variables and mysqli connection

---
+++ includes/db-handler.php
@@ -0,0 +1,17 @@
+<?php
+#using a local server
+$serverName = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "loginbeginner"
+
+$connection = mysqli_connect($serverName.
+$dbUsername,
+$dbPassword,
+$dbName)
+
+if (!$conn) {
+    die("Connection failed: ".mysqli_connect_error())
+}
+
+?>
--- signup.php
+++ signup.php
@@ -5,7 +5,7 @@
     <main>
         <h1>Sign Up</h1>
         
-        <form action="includes/signup.php" method="post">
+        <form action="includes/signup-processing.php" method="post">
             <input type="text" name="uid" placeholder="username">
             <input type="text" name="mail" placeholder="email">
             <input type="password" name="password" placeholder="password">
