3c3b9bf7a5d5f6d1d74f774e12dc60ee9bda1141
A Hashim <ahashim983@yahoo.com>
2020-03-01 09:49:18 +0000

    error messages for the user

--- signup.php
+++ signup.php
@@ -4,7 +4,40 @@
 
     <main>
         <h1>Sign Up</h1>
-        
+        <!-- error messages for user, depending on the error type shown by the url -->
+        <?php
+            if (isset($_GET['error'])) {
+                if ($_GET['error'] == "emptyfields"){
+                    echo '<p> Please complete all fields. </p>';
+                }
+                else if ($_GET['error'] == "invalidmailuid"){
+                    echo '<p> Please enter a valid email address & username. </p>';
+
+                }
+                else if ($_GET['error'] == "invaliduid"){
+                    echo '<p> Please enter a valid username. </p>';
+
+                }
+                else if ($_GET['error'] == "invalidmail"){
+                    echo '<p> Please complete all fields. </p>';
+
+                } 
+                else if ($_GET['error'] == "passwordcheck"){
+                    echo '<p> Passwords do not match. </p>';
+
+                }
+                else if ($_GET['error'] == "usertaken"){
+                    echo '<p> Username already exists. </p>';
+
+                }
+
+            } else {
+                echo '<p> Signed up successfully! </p>';
+
+            }
+
+            ?>
+       
         <form action="includes/signup-processing.php" method="post">
             <input type="text" name="uid" placeholder="username">
             <input type="text" name="mail" placeholder="email">
